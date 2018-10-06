4f46a2ada8f5855f7a7f2aee25dbe562db2c6a4b
rupertlion <rupertlion@gmail.com>
2018-10-06 09:54:32 +0200

    Adding initial address book tests in app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,10 +5,28 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to address-book-angular!');
+  it('should display create contact header', () => {
+    expect(page.getParagraphText()).toEqual('Create contact');
+  });
+
+  it('should add a new contact', () => {
+    let count = page.cardCount();
+    expect(count).toBe(0);
+
+    page.getField('name').sendKeys('John Doe');
+    page.getField('email').sendKeys('john@craftacademy.se');
+    page.getField('company').sendKeys('Craft Academy');
+    page.getField('role').sendKeys('Tester');
+    page.getField('twitter').sendKeys('@tester');
+    page.getField('location').sendKeys('Gothenburg');
+    page.getField('notes').sendKeys('Some tester guy');
+
+    page.getSubmitButton().click();
+
+    count = page.cardCount();
+    expect(count).toBe(1);
   });
 });
