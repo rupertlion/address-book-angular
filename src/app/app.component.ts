374d4003ea6dbc26c667d141c56702494ed5753c
rupertlion <rupertlion@gmail.com>
2018-10-06 10:05:27 +0200

    Updating address book form in view, connect button to .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'address-book-angular';
+
+  createNewContact() {
+    console.log('Someone clicked me!!!')
+  }
 }
