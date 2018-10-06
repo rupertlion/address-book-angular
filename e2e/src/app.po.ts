78dda1909ad6d65b0e2443da5338f7f91aaaad7b
rupertlion <rupertlion@gmail.com>
2018-10-06 10:00:08 +0200

    Updating create contact header to h1

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,16 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getField(name) {
+    return element(by.name(name));
+  }
+
+  getSubmitButton() {
+    return element(by.buttonText('Submit'));
+  }
+
+  cardCount() {
+    return element.all(by.css('.card')).count();
+  }
 }
