10336a39b9df5ecd093c712eaecc190dfd3cd862
rupertlion <rupertlion@gmail.com>
2018-10-06 10:33:36 +0200

    Adding contact component to app.component.spec - all green

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { Input } from '@angular/core';
+import { ContactComponent } from './contact/contact.component';
 
 describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, ContactComponent
       ],
       imports: [
         FormsModule
